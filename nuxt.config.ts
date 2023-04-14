76e183c62de2002b4859ab183ba15f0314ec6e4a
Darryl <darryl.a.nicholson@gmail.com>
2023-04-14 13:17:46 -0700

    project stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [ '@nuxtjs/tailwindcss'],
+    app: {
+        head: {
+            title: 'Single Page Developer Protfolio',
+            meta: [
+                {
+                    name: 'decription', content: 'Single Page Developer Protfolio for Darryl Nicholson'
+                }
+            ],
+            link: [
+                {
+                    rel: 'init-font', href: 'https://fonts.googleapis.com',
+                }
+            ]
+        }
+    }
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Nav />
+  <Hero />
+  <Skills />
+</template>
+
+<script setup>
+import Nav from "~~/components/nav.vue";
+import Hero from "~~/components/hero.vue";
+import Skills from "~~/components/skills.vue";
+</script>
+
+<style scoped></style>
